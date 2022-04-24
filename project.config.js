07c34b45996f1e5eacb77f3a8a87666e3a3c99db
夜中的兔子 <51472515+1059431281@users.noreply.github.com>
2022-04-24 17:11:47 +0800

    first commit

---
+++ project.config.js
@@ -0,0 +1,12 @@
+let config = {
+    title: "网页标题", // 网页标题
+    useCdnUrl: true, //是否使用svn绝对路径
+    baiduUrl: "", // 百度统计链接
+    cdnUrl: "//cdn.jsdelivr.net/gh/1059431281/Blog-source/img/weixin-dz/",
+    grayCdnUrl: "//cdn.jsdelivr.net/gh/1059431281/Blog-source/img/weixin-dz/",
+    testCdnUrl: "//cdn.jsdelivr.net/gh/1059431281/Blog-source/img/weixin-dz/",
+    innerCdnUrl: "//cdn.jsdelivr.net/gh/1059431281/Blog-source/img/weixin-dz/",
+    forceHttps: false,
+};
+
+module.exports = config;
